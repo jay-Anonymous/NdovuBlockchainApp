2b156e5e4a609c1042c42308a7c232d756e39b7b
wechman <damian.wechman.it@gmail.com>
2022-07-06 08:57:37 +0200

    fixup! Added few tests.

--- test/libsolidity/syntaxTests/operators/custom/operator_not_user_implemented.sol
+++ test/libsolidity/syntaxTests/operators/custom/operator_not_user_implemented.sol
@@ -4,7 +4,7 @@ using {
 } for int256;
 
 // ----
-// ParserError 1885: (22-24): The operator << cannot be user-implemented. This is only possible for the folloing operators: |, &, ^, +, -, *, /, %, ==, !=, <, >, <=, >=, ~
-// ParserError 1885: (36-38): The operator >> cannot be user-implemented. This is only possible for the folloing operators: |, &, ^, +, -, *, /, %, ==, !=, <, >, <=, >=, ~
-// ParserError 1885: (51-53): The operator ** cannot be user-implemented. This is only possible for the folloing operators: |, &, ^, +, -, *, /, %, ==, !=, <, >, <=, >=, ~
-// ParserError 1885: (62-63): The operator ! cannot be user-implemented. This is only possible for the folloing operators: |, &, ^, +, -, *, /, %, ==, !=, <, >, <=, >=, ~
+// ParserError 1885: (22-24): The operator << cannot be user-implemented. This is only possible for the following operators: |, &, ^, +, -, *, /, %, ==, !=, <, >, <=, >=, ~
+// ParserError 1885: (36-38): The operator >> cannot be user-implemented. This is only possible for the following operators: |, &, ^, +, -, *, /, %, ==, !=, <, >, <=, >=, ~
+// ParserError 1885: (51-53): The operator ** cannot be user-implemented. This is only possible for the following operators: |, &, ^, +, -, *, /, %, ==, !=, <, >, <=, >=, ~
+// ParserError 1885: (62-63): The operator ! cannot be user-implemented. This is only possible for the following operators: |, &, ^, +, -, *, /, %, ==, !=, <, >, <=, >=, ~
