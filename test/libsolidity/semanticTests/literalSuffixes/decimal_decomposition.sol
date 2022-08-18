e19908337ec4a58535d4bddaa8e21b0a009596a0
Kamil Śliwak <kamil.sliwak@codepoets.it>
2022-08-18 13:17:56 +0200

    [TMP] Semantic tests

---
+++ test/libsolidity/semanticTests/literalSuffixes/decimal_decomposition.sol
@@ -0,0 +1,33 @@
+struct Decimal {
+    uint mantissa;
+    uint exponent;
+}
+
+function asDecimal(uint mantissa, uint exponent) pure returns (Decimal memory) {
+    return Decimal(mantissa, exponent);
+}
+
+contract C {
+    function simple() public pure returns (Decimal memory, Decimal memory, Decimal memory, Decimal memory, Decimal memory) {
+        return (
+            1234567800.0 asDecimal,
+            12345678.0 asDecimal,
+            1234.5678 asDecimal,
+            12.345678 asDecimal,
+            0.12345678 asDecimal
+        );
+    }
+
+    function maxMantissa() public pure returns (Decimal memory) {
+        return 0.00115792089237316195423570985008687907853269984665640564039457584007913129639935e10 asDecimal;  // (2**256 - 1) * 10**-(80 - 10)
+    }
+
+    function maxUint8Exponent() public pure returns (Decimal memory) {
+        return 1e-256 asDecimal;
+    }
+}
+
+// ----
+// simple() -> 1234567800, 0, 12345678, 0, 12345678, 4, 12345678, 6, 12345678, 8
+// maxMantissa() -> 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff, 70
+// maxUint8Exponent() -> 1, 256
