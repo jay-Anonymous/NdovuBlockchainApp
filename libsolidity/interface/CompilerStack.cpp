aarlt/solidity

Basic nlohmann-json integration.

libsolidity/interface/CompilerStack.cpp
@@ -50,8 +50,6 @@
 #include <libdevcore/SwarmHash.h>
 #include <libdevcore/JSON.h>
 
-#include <json/json.h>
-
 #include <boost/algorithm/string.hpp>
 
 using namespace std;
@@ -412,13 +410,13 @@ string CompilerStack::assemblyString(string const& _contractName, StringMap _sou
 }
 
 /// FIXME: cache the JSON
-Json::Value CompilerStack::assemblyJSON(string const& _contractName, StringMap _sourceCodes) const
+Json CompilerStack::assemblyJSON(string const& _contractName, StringMap _sourceCodes) const
 {
 	Contract const& currentContract = contract(_contractName);
 	if (currentContract.compiler)
 		return currentContract.compiler->assemblyJSON(_sourceCodes);
 	else
-		return Json::Value();
+		return Json();
 }
 
 vector<string> CompilerStack::sourceNames() const
@@ -438,12 +436,12 @@ map<string, unsigned> CompilerStack::sourceIndices() const
 	return indices;
 }
 
-Json::Value const& CompilerStack::contractABI(string const& _contractName) const
+Json const& CompilerStack::contractABI(string const& _contractName) const
 {
 	return contractABI(contract(_contractName));
 }
 
-Json::Value const& CompilerStack::contractABI(Contract const& _contract) const
+Json const& CompilerStack::contractABI(Contract const& _contract) const
 {
 	if (m_stackState < AnalysisSuccessful)
 		BOOST_THROW_EXCEPTION(CompilerError() << errinfo_comment("Parsing was not successful."));
@@ -452,17 +450,17 @@ Json::Value const& CompilerStack::contractABI(Contract const& _contract) const
 
 	// caches the result
 	if (!_contract.abi)
-		_contract.abi.reset(new Json::Value(ABI::generate(*_contract.contract)));
+		_contract.abi.reset(new Json(ABI::generate(*_contract.contract)));
 
 	return *_contract.abi;
 }
 
-Json::Value const& CompilerStack::natspecUser(string const& _contractName) const
+Json const& CompilerStack::natspecUser(string const& _contractName) const
 {
 	return natspecUser(contract(_contractName));
 }
 
-Json::Value const& CompilerStack::natspecUser(Contract const& _contract) const
+Json const& CompilerStack::natspecUser(Contract const& _contract) const
 {
 	if (m_stackState < AnalysisSuccessful)
 		BOOST_THROW_EXCEPTION(CompilerError() << errinfo_comment("Parsing was not successful."));
@@ -471,17 +469,17 @@ Json::Value const& CompilerStack::natspecUser(Contract const& _contract) const
 
 	// caches the result
 	if (!_contract.userDocumentation)
-		_contract.userDocumentation.reset(new Json::Value(Natspec::userDocumentation(*_contract.contract)));
+		_contract.userDocumentation.reset(new Json(Natspec::userDocumentation(*_contract.contract)));
 
 	return *_contract.userDocumentation;
 }
 
-Json::Value const& CompilerStack::natspecDev(string const& _contractName) const
+Json const& CompilerStack::natspecDev(string const& _contractName) const
 {
 	return natspecDev(contract(_contractName));
 }
 
-Json::Value const& CompilerStack::natspecDev(Contract const& _contract) const
+Json const& CompilerStack::natspecDev(Contract const& _contract) const
 {
 	if (m_stackState < AnalysisSuccessful)
 		BOOST_THROW_EXCEPTION(CompilerError() << errinfo_comment("Parsing was not successful."));
@@ -490,14 +488,14 @@ Json::Value const& CompilerStack::natspecDev(Contract const& _contract) const
 
 	// caches the result
 	if (!_contract.devDocumentation)
-		_contract.devDocumentation.reset(new Json::Value(Natspec::devDocumentation(*_contract.contract)));
+		_contract.devDocumentation.reset(new Json(Natspec::devDocumentation(*_contract.contract)));
 
 	return *_contract.devDocumentation;
 }
 
-Json::Value CompilerStack::methodIdentifiers(string const& _contractName) const
+Json CompilerStack::methodIdentifiers(string const& _contractName) const
 {
-	Json::Value methodIdentifiers(Json::objectValue);
+	Json methodIdentifiers(Json::object());
 	for (auto const& it: contractDefinition(_contractName).interfaceFunctions())
 		methodIdentifiers[it.second->externalSignature()] = toHex(it.first.ref());
 	return methodIdentifiers;
@@ -840,7 +838,7 @@ CompilerStack::Source const& CompilerStack::source(string const& _sourceName) co
 
 string CompilerStack::createMetadata(Contract const& _contract) const
 {
-	Json::Value meta;
+	Json meta;
 	meta["version"] = 1;
 	meta["language"] = "Solidity";
 	meta["compiler"]["version"] = VersionStringStrict;
@@ -851,7 +849,7 @@ string CompilerStack::createMetadata(Contract const& _contract) const
 	for (auto const sourceUnit: _contract.contract->sourceUnit().referencedSourceUnits(true))
 		referencedSources.insert(sourceUnit->annotation().path);
 
-	meta["sources"] = Json::objectValue;
+	meta["sources"] = Json::object();
 	for (auto const& s: m_sources)
 	{
 		if (!referencedSources.count(s.first))
@@ -864,8 +862,8 @@ string CompilerStack::createMetadata(Contract const& _contract) const
 			meta["sources"][s.first]["content"] = s.second.scanner->source();
 		else
 		{
-			meta["sources"][s.first]["urls"] = Json::arrayValue;
-			meta["sources"][s.first]["urls"].append(
+			meta["sources"][s.first]["urls"] = Json::array();
+			meta["sources"][s.first]["urls"].emplace_back(
 				"bzzr://" + toHex(dev::swarmHash(s.second.scanner->source()).asBytes())
 			);
 		}
@@ -876,14 +874,14 @@ string CompilerStack::createMetadata(Contract const& _contract) const
 	meta["settings"]["compilationTarget"][_contract.contract->sourceUnitName()] =
 		_contract.contract->annotation().canonicalName;
 
-	meta["settings"]["remappings"] = Json::arrayValue;
+	meta["settings"]["remappings"] = Json::array();
 	set<string> remappings;
 	for (auto const& r: m_remappings)
 		remappings.insert(r.context + ":" + r.prefix + "=" + r.target);
 	for (auto const& r: remappings)
-		meta["settings"]["remappings"].append(r);
+		meta["settings"]["remappings"].emplace_back(r);
 
-	meta["settings"]["libraries"] = Json::objectValue;
+	meta["settings"]["libraries"] = Json::object();
 	for (auto const& library: m_libraries)
 		meta["settings"]["libraries"][library.first] = "0x" + toHex(library.second.asBytes());
 
@@ -970,24 +968,24 @@ string CompilerStack::computeSourceMapping(eth::AssemblyItems const& _items) con
 namespace
 {
 
-Json::Value gasToJson(GasEstimator::GasConsumption const& _gas)
+Json gasToJson(GasEstimator::GasConsumption const& _gas)
 {
 	if (_gas.isInfinite)
-		return Json::Value("infinite");
+		return Json("infinite");
 	else
-		return Json::Value(toString(_gas.value));
+		return Json(_gas.value.str());
 }
 
 }
 
-Json::Value CompilerStack::gasEstimates(string const& _contractName) const
+Json CompilerStack::gasEstimates(string const& _contractName) const
 {
 	if (!assemblyItems(_contractName) && !runtimeAssemblyItems(_contractName))
-		return Json::Value();
+		return Json();
 
 	using Gas = GasEstimator::GasConsumption;
 	GasEstimator gasEstimator(m_evmVersion);
-	Json::Value output(Json::objectValue);
+	Json output(Json::object());
 
 	if (eth::AssemblyItems const* items = assemblyItems(_contractName))
 	{
@@ -995,7 +993,7 @@ Json::Value CompilerStack::gasEstimates(string const& _contractName) const
 		u256 bytecodeSize(runtimeObject(_contractName).bytecode.size());
 		Gas codeDepositGas = bytecodeSize * eth::GasCosts::createDataGas;
 
-		Json::Value creation(Json::objectValue);
+		Json creation(Json::object());
 		creation["codeDepositCost"] = gasToJson(codeDepositGas);
 		creation["executionCost"] = gasToJson(executionGas);
 		/// TODO: implement + overload to avoid the need of +=
@@ -1008,7 +1006,7 @@ Json::Value CompilerStack::gasEstimates(string const& _contractName) const
 	{
 		/// External functions
 		ContractDefinition const& contract = contractDefinition(_contractName);
-		Json::Value externalFunctions(Json::objectValue);
+		Json externalFunctions(Json::object());
 		for (auto it: contract.interfaceFunctions())
 		{
 			string sig = it.second->externalSignature();
@@ -1025,7 +1023,7 @@ Json::Value CompilerStack::gasEstimates(string const& _contractName) const
 			output["external"] = externalFunctions;
 
 		/// Internal functions
-		Json::Value internalFunctions(Json::objectValue);
+		Json internalFunctions(Json::object());
 		for (auto const& it: contract.definedFunctions())
 		{
 			/// Exclude externally visible functions, constructor and the fallback function

jay-Anonymous/NdovuBlockchainApp

Co-authored-by: Alex Beregszaszi <alex@rtfs.hu>

libsolidity/interface/CompilerStack.cpp
@@ -77,8 +77,6 @@
 #include <libsolutil/Algorithms.h>
 #include <libsolutil/FunctionSelector.h>
 
-#include <json/json.h>
-
 #include <boost/algorithm/string/replace.hpp>
 
 #include <range/v3/view/concat.hpp>
@@ -391,7 +389,7 @@ bool CompilerStack::parse()
 	return !m_hasError;
 }
 
-void CompilerStack::importASTs(map<string, Json::Value> const& _sources)
+void CompilerStack::importASTs(map<string, Json> const& _sources)
 {
 	if (m_stackState != Empty)
 		solThrow(CompilerError, "Must call importASTs only before the SourcesSet state.");
@@ -758,18 +756,18 @@ evmasm::AssemblyItems const* CompilerStack::runtimeAssemblyItems(string const& _
 	return currentContract.evmRuntimeAssembly ? &currentContract.evmRuntimeAssembly->items() : nullptr;
 }
 
-Json::Value CompilerStack::generatedSources(string const& _contractName, bool _runtime) const
+Json CompilerStack::generatedSources(string const& _contractName, bool _runtime) const
 {
 	if (m_stackState != CompilationSuccessful)
 		solThrow(CompilerError, "Compilation was not successful.");
 
 	Contract const& c = contract(_contractName);
-	util::LazyInit<Json::Value const> const& sources =
+	util::LazyInit<Json const> const& sources =
 		_runtime ?
 		c.runtimeGeneratedSources :
 		c.generatedSources;
 	return sources.init([&]{
-		Json::Value sources{Json::arrayValue};
+		Json sources{Json::array()};
 		// If there is no compiler, then no bytecode was generated and thus no
 		// sources were generated (or we compiled "via IR").
 		if (c.compiler)
@@ -917,7 +915,7 @@ string CompilerStack::assemblyString(string const& _contractName, StringMap cons
 }
 
 /// TODO: cache the JSON
-Json::Value CompilerStack::assemblyJSON(string const& _contractName) const
+Json CompilerStack::assemblyJSON(string const& _contractName) const
 {
 	if (m_stackState != CompilationSuccessful)
 		solThrow(CompilerError, "Compilation was not successful.");
@@ -926,7 +924,7 @@ Json::Value CompilerStack::assemblyJSON(string const& _contractName) const
 	if (currentContract.evmAssembly)
 		return currentContract.evmAssembly->assemblyJSON(sourceIndices());
 	else
-		return Json::Value();
+		return Json();
 }
 
 vector<string> CompilerStack::sourceNames() const
@@ -948,7 +946,7 @@ map<string, unsigned> CompilerStack::sourceIndices() const
 	return indices;
 }
 
-Json::Value const& CompilerStack::contractABI(string const& _contractName) const
+Json const& CompilerStack::contractABI(string const& _contractName) const
 {
 	if (m_stackState < AnalysisPerformed)
 		solThrow(CompilerError, "Analysis was not successful.");
@@ -956,7 +954,7 @@ Json::Value const& CompilerStack::contractABI(string const& _contractName) const
 	return contractABI(contract(_contractName));
 }
 
-Json::Value const& CompilerStack::contractABI(Contract const& _contract) const
+Json const& CompilerStack::contractABI(Contract const& _contract) const
 {
 	if (m_stackState < AnalysisPerformed)
 		solThrow(CompilerError, "Analysis was not successful.");
@@ -966,7 +964,7 @@ Json::Value const& CompilerStack::contractABI(Contract const& _contract) const
 	return _contract.abi.init([&]{ return ABI::generate(*_contract.contract); });
 }
 
-Json::Value const& CompilerStack::storageLayout(string const& _contractName) const
+Json const& CompilerStack::storageLayout(string const& _contractName) const
 {
 	if (m_stackState < AnalysisPerformed)
 		solThrow(CompilerError, "Analysis was not successful.");
@@ -974,7 +972,7 @@ Json::Value const& CompilerStack::storageLayout(string const& _contractName) con
 	return storageLayout(contract(_contractName));
 }
 
-Json::Value const& CompilerStack::storageLayout(Contract const& _contract) const
+Json const& CompilerStack::storageLayout(Contract const& _contract) const
 {
 	if (m_stackState < AnalysisPerformed)
 		solThrow(CompilerError, "Analysis was not successful.");
@@ -984,7 +982,7 @@ Json::Value const& CompilerStack::storageLayout(Contract const& _contract) const
 	return _contract.storageLayout.init([&]{ return StorageLayout().generate(*_contract.contract); });
 }
 
-Json::Value const& CompilerStack::natspecUser(string const& _contractName) const
+Json const& CompilerStack::natspecUser(string const& _contractName) const
 {
 	if (m_stackState < AnalysisPerformed)
 		solThrow(CompilerError, "Analysis was not successful.");
@@ -992,7 +990,7 @@ Json::Value const& CompilerStack::natspecUser(string const& _contractName) const
 	return natspecUser(contract(_contractName));
 }
 
-Json::Value const& CompilerStack::natspecUser(Contract const& _contract) const
+Json const& CompilerStack::natspecUser(Contract const& _contract) const
 {
 	if (m_stackState < AnalysisPerformed)
 		solThrow(CompilerError, "Analysis was not successful.");
@@ -1002,7 +1000,7 @@ Json::Value const& CompilerStack::natspecUser(Contract const& _contract) const
 	return _contract.userDocumentation.init([&]{ return Natspec::userDocumentation(*_contract.contract); });
 }
 
-Json::Value const& CompilerStack::natspecDev(string const& _contractName) const
+Json const& CompilerStack::natspecDev(string const& _contractName) const
 {
 	if (m_stackState < AnalysisPerformed)
 		solThrow(CompilerError, "Analysis was not successful.");
@@ -1010,7 +1008,7 @@ Json::Value const& CompilerStack::natspecDev(string const& _contractName) const
 	return natspecDev(contract(_contractName));
 }
 
-Json::Value const& CompilerStack::natspecDev(Contract const& _contract) const
+Json const& CompilerStack::natspecDev(Contract const& _contract) const
 {
 	if (m_stackState < AnalysisPerformed)
 		solThrow(CompilerError, "Analysis was not successful.");
@@ -1020,14 +1018,14 @@ Json::Value const& CompilerStack::natspecDev(Contract const& _contract) const
 	return _contract.devDocumentation.init([&]{ return Natspec::devDocumentation(*_contract.contract); });
 }
 
-Json::Value CompilerStack::interfaceSymbols(string const& _contractName) const
+Json CompilerStack::interfaceSymbols(string const& _contractName) const
 {
 	if (m_stackState < AnalysisPerformed)
 		solThrow(CompilerError, "Analysis was not successful.");
 
-	Json::Value interfaceSymbols(Json::objectValue);
+	Json interfaceSymbols(Json::object());
 	// Always have a methods object
-	interfaceSymbols["methods"] = Json::objectValue;
+	interfaceSymbols["methods"] = Json::object();
 
 	for (auto const& it: contractDefinition(_contractName).interfaceFunctions())
 		interfaceSymbols["methods"][it.second->externalSignature()] = it.first.hex();
@@ -1480,7 +1478,7 @@ CompilerStack::Source const& CompilerStack::source(string const& _sourceName) co
 
 string CompilerStack::createMetadata(Contract const& _contract, bool _forIR) const
 {
-	Json::Value meta{Json::objectValue};
+	Json meta{Json::object()};
 	meta["version"] = 1;
 	meta["language"] = m_importedSources ? "SolidityAST" : "Solidity";
 	meta["compiler"]["version"] = VersionStringStrict;
@@ -1491,7 +1489,7 @@ string CompilerStack::createMetadata(Contract const& _contract, bool _forIR) con
 	for (auto const sourceUnit: _contract.contract->sourceUnit().referencedSourceUnits(true))
 		referencedSources.insert(*sourceUnit->annotation().path);
 
-	meta["sources"] = Json::objectValue;
+	meta["sources"] = Json::object();
 	for (auto const& s: m_sources)
 	{
 		if (!referencedSources.count(s.first))
@@ -1505,15 +1503,15 @@ string CompilerStack::createMetadata(Contract const& _contract, bool _forIR) con
 			meta["sources"][s.first]["content"] = s.second.charStream->source();
 		else
 		{
-			meta["sources"][s.first]["urls"] = Json::arrayValue;
-			meta["sources"][s.first]["urls"].append("bzz-raw://" + util::toHex(s.second.swarmHash().asBytes()));
-			meta["sources"][s.first]["urls"].append(s.second.ipfsUrl());
+			meta["sources"][s.first]["urls"] = Json::array();
+			meta["sources"][s.first]["urls"].emplace_back("bzz-raw://" + util::toHex(s.second.swarmHash().asBytes()));
+			meta["sources"][s.first]["urls"].emplace_back(s.second.ipfsUrl());
 		}
 	}
 
-	static_assert(sizeof(m_optimiserSettings.expectedExecutionsPerDeployment) <= sizeof(Json::LargestUInt), "Invalid word size.");
-	solAssert(static_cast<Json::LargestUInt>(m_optimiserSettings.expectedExecutionsPerDeployment) < std::numeric_limits<Json::LargestUInt>::max(), "");
-	meta["settings"]["optimizer"]["runs"] = Json::Value(Json::LargestUInt(m_optimiserSettings.expectedExecutionsPerDeployment));
+	static_assert(sizeof(m_optimiserSettings.expectedExecutionsPerDeployment) <= sizeof(Json::number_integer_t), "Invalid word size.");
+	solAssert(static_cast<Json::number_integer_t>(m_optimiserSettings.expectedExecutionsPerDeployment) < std::numeric_limits<Json::number_integer_t>::max(), "");
+	meta["settings"]["optimizer"]["runs"] = Json::number_integer_t(m_optimiserSettings.expectedExecutionsPerDeployment);
 
 	/// Backwards compatibility: If set to one of the default settings, do not provide details.
 	OptimiserSettings settingsWithoutRuns = m_optimiserSettings;
@@ -1525,7 +1523,7 @@ string CompilerStack::createMetadata(Contract const& _contract, bool _forIR) con
 		meta["settings"]["optimizer"]["enabled"] = true;
 	else
 	{
-		Json::Value details{Json::objectValue};
+		Json details{Json::object()};
 
 		details["orderLiterals"] = m_optimiserSettings.runOrderLiterals;
 		details["inliner"] = m_optimiserSettings.runInliner;
@@ -1537,7 +1535,7 @@ string CompilerStack::createMetadata(Contract const& _contract, bool _forIR) con
 		details["yul"] = m_optimiserSettings.runYulOptimiser;
 		if (m_optimiserSettings.runYulOptimiser)
 		{
-			details["yulDetails"] = Json::objectValue;
+			details["yulDetails"] = Json::object();
 			details["yulDetails"]["stackAllocation"] = m_optimiserSettings.optimizeStackAllocation;
 			details["yulDetails"]["optimizerSteps"] = m_optimiserSettings.yulOptimiserSteps;
 		}
@@ -1560,14 +1558,14 @@ string CompilerStack::createMetadata(Contract const& _contract, bool _forIR) con
 	meta["settings"]["compilationTarget"][_contract.contract->sourceUnitName()] =
 		*_contract.contract->annotation().canonicalName;
 
-	meta["settings"]["remappings"] = Json::arrayValue;
+	meta["settings"]["remappings"] = Json::array();
 	set<string> remappings;
 	for (auto const& r: m_importRemapper.remappings())
 		remappings.insert(r.context + ":" + r.prefix + "=" + r.target);
 	for (auto const& r: remappings)
-		meta["settings"]["remappings"].append(r);
+		meta["settings"]["remappings"].emplace_back(r);
 
-	meta["settings"]["libraries"] = Json::objectValue;
+	meta["settings"]["libraries"] = Json::object();
 	for (auto const& library: m_libraries)
 		meta["settings"]["libraries"][library.first] = "0x" + util::toHex(library.second.asBytes());
 
@@ -1699,34 +1697,35 @@ bytes CompilerStack::createCBORMetadata(Contract const& _contract, bool _forIR) 
 namespace
 {
 
-Json::Value gasToJson(GasEstimator::GasConsumption const& _gas)
+Json gasToJson(GasEstimator::GasConsumption const& _gas)
 {
 	if (_gas.isInfinite)
-		return Json::Value("infinite");
+		return Json("infinite");
 	else
-		return Json::Value(util::toString(_gas.value));
+		// TODO use _gas.value.str()?
+		return Json(util::toString(_gas.value));
 }
 
 }
 
-Json::Value CompilerStack::gasEstimates(string const& _contractName) const
+Json CompilerStack::gasEstimates(string const& _contractName) const
 {
 	if (m_stackState != CompilationSuccessful)
 		solThrow(CompilerError, "Compilation was not successful.");
 
 	if (!assemblyItems(_contractName) && !runtimeAssemblyItems(_contractName))
-		return Json::Value();
+		return Json();
 
 	using Gas = GasEstimator::GasConsumption;
 	GasEstimator gasEstimator(m_evmVersion);
-	Json::Value output(Json::objectValue);
+	Json output(Json::object());
 
 	if (evmasm::AssemblyItems const* items = assemblyItems(_contractName))
 	{
 		Gas executionGas = gasEstimator.functionalEstimation(*items);
 		Gas codeDepositGas{evmasm::GasMeter::dataGas(runtimeObject(_contractName).bytecode, false, m_evmVersion)};
 
-		Json::Value creation(Json::objectValue);
+		Json creation(Json::object());
 		creation["codeDepositCost"] = gasToJson(codeDepositGas);
 		creation["executionCost"] = gasToJson(executionGas);
 		/// TODO: implement + overload to avoid the need of +=
@@ -1739,7 +1738,7 @@ Json::Value CompilerStack::gasEstimates(string const& _contractName) const
 	{
 		/// External functions
 		ContractDefinition const& contract = contractDefinition(_contractName);
-		Json::Value externalFunctions(Json::objectValue);
+		Json externalFunctions(Json::object());
 		for (auto it: contract.interfaceFunctions())
 		{
 			string sig = it.second->externalSignature();
@@ -1756,7 +1755,7 @@ Json::Value CompilerStack::gasEstimates(string const& _contractName) const
 			output["external"] = externalFunctions;
 
 		/// Internal functions
-		Json::Value internalFunctions(Json::objectValue);
+		Json internalFunctions(Json::object());
 		for (auto const& it: contract.definedFunctions())
 		{
 			/// Exclude externally visible functions, constructor, fallback and receive ether function